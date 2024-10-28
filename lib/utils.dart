39f7ce8b0e127234b5b093c18ec93335bf437dbb
Akhdan Habibie <60088854+Akhdan1998@users.noreply.github.com>
2024-10-28 09:31:14 +0700

    token agora

--- lib/utils.dart
+++ lib/utils.dart
@@ -89,18 +89,18 @@ String genereteChatID({required String uid1, required String uid2}) {
 
 String appIdGroupVideo = 'de71d649f3e24489b4b66acd07983a96';
 String tokenGroupVideo =
-    '007eJxTYDjbmqm7PE/TM7FEiDfTkGFCrsDafQyut/2uHzY/pbU7+7YCQ0qquWGKmYllmnGqkYmJhWWSSZKZWWJyioG5pYVxoqXZux+S6Q2BjAweZSeYGRkgEMRnYygtTi3yTGFgAADk2B6b';
+    '007eJxTYGhXT/75sGu6tYHJp/u/r4W53rcyLlyYfJVb8kea1MU5k3YqMKSkmhummJlYphmnGpmYWFgmmSSZmSUmpxiYW1oYJ1qaHXgvl94QyMjQcMOElZEBAkF8NobS4tQizxQGBgD/yCGa';
 
 String appIdGroupAudio = '3cdecaec34ea49c38355b28f5d803ac0';
 String tokenGroupAudio =
-    '007eJxTYDDM97+26S9/xhwjRsMAUXXT5K42O7aip3eO2/+paS7Nf63AYJyckpqcmJpsbJKaaGKZbGxhbGqaZGSRZppiYWCcmGwg/FMyvSGQkeHRrzxmRgYIBPHZGEqLU4s8UxgYADx5H+Q=';
+    '007eJxTYMhVlljxRr1De0f/mnvHuZotJ+/98Zwpa9ItwZUn9C03fZJQYDBOTklNTkxNNjZJTTSxTDa2MDY1TTKySDNNsTAwTkw2ePteLr0hkJGBhaGJgREKQXw2htLi1CLPFAYGAMwdIK0=';
 
 String appIdAudio = 'fecb15824e0c4e888662459c3b74c273';
 String tokenAudio =
-    '007eJxTYNBxbbx6b/6S3u2XfM1sFUqUs5j/t0Y3KfZvslP4F/ZO+5YCQ1pqcpKhqYWRSapBskmqhYWFmZmRiallsnGSuUmykbmx/k/J9IZARga+fcdZGBkgEMRnYygtTi3yTGFgAABg1B9T';
+    '007eJxTYLgjWSnqtI67eVbtr+bzG3jnTgqQSbvC9JwrMl0yNHX3H2kFhrTU5CRDUwsjk1SDZJNUCwsLMzMjE1PLZOMkc5NkI3Nj3Q9y6Q2BjAxTa5WYGBkgEMRnYygtTi3yTGFgAAAIOh3w';
 
 String appIdVideo = 'be76645285084ce7a1d7d4cd2bd94dd0';
 String tokenVideo =
-    '007eJxTYFinz6afk67VZiNd4jf7QVNKZnXaj6o7AptKOmbV3+fZxqPAkJRqbmZmYmpkYWpgYZKcap5omGKeYpKcYpSUYmmSkmLg+VMyvSGQkSF8yTYWRgYIBPHZGEqLU4s8UxgYAAQzHvM=';
+    '007eJxTYOg4LKDrlpjgsXGuoZyMT86bHgO/HvcrvCx/0+7OcZHtYlZgSEk1N0wxM7FMM041MjGxsEwySTIzS0xOMTC3tDBOtDSr/SCX3hDIyKD3ppyVkQECQXw2htLi1CLPFAYGAJplHUs=';
 
 String channel = "userId";
